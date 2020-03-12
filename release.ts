e61761b73c304059e904220aab25415c68b53146
Fly me to the moon <git@bigly.dog>
2020-03-12 02:25:18 -0700

    fix release

--- release.ts
+++ release.ts
@@ -5,21 +5,11 @@ import { run } from "nda/dist/node/sub_process"
 
 const dist_dir = "./dist"
 const artifacts_dir = "./artifacts"
-const sub_path = "/kaleidoscope-page"
 
 const main = async () => {
-  await rm(dist_dir)
-  const { stdout, stderr } = await run(
-    "parcel",
-    "--public-url",
-    sub_path,
-    "src/index.html",
-  )
+  const { stdout, stderr } = await run("npm", "run", "build")
   console.error(stderr)
   console.log(stdout)
-  const { stdout: goout, stderr: goerr } = await run("./build.sh")
-  console.error(goerr)
-  console.log(goout)
   const prev = await readdir(1, artifacts_dir)
   const prev__artifacts = filter((n) => !n.startsWith(".git"), [
     ...prev.dirs,
