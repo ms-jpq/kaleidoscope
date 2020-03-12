85fad783e1311131e61c1e1efdac57726a8dbc99
Fly me to the moon <git@bigly.dog>
2020-03-12 02:32:06 -0700

    fixed release

--- release.ts
+++ release.ts
@@ -11,7 +11,7 @@ const main = async () => {
   console.error(stderr)
   console.log(stdout)
   const prev = await readdir(1, artifacts_dir)
-  const prev__artifacts = filter((n) => !n.startsWith(".git"), [
+  const prev__artifacts = filter((n) => !n.endsWith(".git"), [
     ...prev.dirs,
     ...prev.files,
   ])
